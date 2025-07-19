86c4c7f3b27d9bfb79065a1b9773759a64d96a5b
Vitor Sales <vitormalisa@gmail.com>
2025-07-19 17:02:54 -0300

    feat: Add flex layout to SuperHeader and Header components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -27,11 +27,11 @@ const Header = () => {
 };
 
 const MainHeader = styled.div`
-  padding: 0 32px;
+  padding: 16px 32px;
   border-bottom: 1px solid ${COLORS.gray[300]};
 
   display: flex;
-
+  align-items: center;
 `;
 
 const Nav = styled.nav`
